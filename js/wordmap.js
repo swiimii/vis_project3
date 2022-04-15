f4a028345c7ba0897ab11e7e3b548611416872ff
Jenn <97474493+jenngutman4@users.noreply.github.com>
2022-04-15 13:38:54 -0400

    Update wordmap.js

--- js/wordmap.js
+++ js/wordmap.js
@@ -1,21 +1,22 @@
 class Wordmap {
 	constructor(_config, _data) {
-    this.config = {
-      parentElement: _config.parentElement,
-      containerWidth: _config.containerWidth || 500,
-      containerHeight: _config.containerHeight || 140,
-      margin: { top: 15, bottom: 50, right: 10, left: 50 }
-    }
+	    this.config = {
+	      parentElement: _config.parentElement,
+	      containerWidth: _config.containerWidth || 500,
+	      containerHeight: _config.containerHeight || 140,
+	      margin: { top: 15, bottom: 50, right: 10, left: 50 }
+	    }
 
-    this.data = _data;
+	    this.data = _data;
 
-    // Call a class function
-    this.initVis();
-}
+	    // Call a class function
+	    this.initVis();
+	}
 
-initVis() {
+	initVis() {
 
-}
-updateVis() {
-	
+	}
+	updateVis() {
+
+	}
 }
